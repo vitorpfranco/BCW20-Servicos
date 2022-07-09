878dc5b49c98069e9a88d8582f816df05c6d8a57
José Almir <jr.cod.dev@gmail.com>
2022-07-08 23:09:12 -0300

    Scheduling

--- Servicos/src/main/java/com/soulcode/Servicos/ServicosApplication.java
+++ Servicos/src/main/java/com/soulcode/Servicos/ServicosApplication.java
@@ -4,14 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableCaching
+@EnableScheduling
 @SpringBootApplication
 public class ServicosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServicosApplication.class, args);
+    }
+
+    @Scheduled(cron = "0 0 0 ? * ?")
+    public void cronJobSch() throws Exception {
+        System.out.println("Rotina diária do servidor...");
     }
 }
