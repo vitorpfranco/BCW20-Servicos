d3266d7f14dd65ad6ec06e74664c480926638597
José Almir <jr.cod.dev@gmail.com>
2022-07-12 15:19:41 -0300

    Aula 12-07 parte 1

--- Servicos/src/main/java/com/soulcode/Servicos/ServicosApplication.java
+++ Servicos/src/main/java/com/soulcode/Servicos/ServicosApplication.java
@@ -2,6 +2,9 @@ package com.soulcode.Servicos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ServicosApplication {
@@ -10,4 +13,8 @@ public class ServicosApplication {
 		SpringApplication.run(ServicosApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
